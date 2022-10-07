20072d91600f284c18e060214dd256df6de5242f
heora <yueluo.yang@gmail.com>
2022-10-07 11:04:32 +0800

    feat: file blob slice

--- file/blob-slice/index.js
+++ file/blob-slice/index.js
@@ -1,1 +1,29 @@
- 
+const path = require('node:path')
+const Koa = require('koa')
+const Router = require('koa-router')
+const serve = require('koa-static')
+const parser = require('koa-bodyparser')
+const multiparty = require('multiparty')
+
+const app = new Koa()
+const router = new Router()
+
+router.post('/upload', async (ctx, next) => {
+  const form = new multiparty.Form({
+    uploadDir: 'temp'
+  })
+
+  form.parse(ctx.req)
+
+  form.on('file', () => {})
+
+  ctx.body = 'upload success'
+})
+
+app.use(parser())
+app.use(serve(path.resolve(__dirname, 'public')))
+app.use(router.routes())
+
+app.listen(3000, () => {
+  console.log('server listen at port 3000...')
+})
